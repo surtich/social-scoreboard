f68347b59a8791944429bb125566e289a25131bf
IGZJavierPerez <surtich@gmail.com>
2014-09-15 12:21:34 +0200

    Implemented push notifications

--- server/config/openshift.js
+++ server/config/openshift.js
@@ -4,6 +4,7 @@ module.exports = {
 		port: process.env.OPENSHIFT_NODEJS_PORT || 8080
 	},
 	db: {
-		conn: process.env.OPENSHIFT_MONGODB_DB_URL + '/socialscoreboard'
+		//conn: process.env.OPENSHIFT_MONGODB_DB_URL + '/socialscoreboard'
+		conn: 'mongodb://user:123456@ds035260.mongolab.com:35260/socialscoreboard'
 	}
 };
--- server/server.js
+++ server/server.js
@@ -8,12 +8,14 @@ var fs = require('fs');
 var config = require('./util/config');
 
 var app = express();
+var http = require('http').Server(app);
+
+var io = require('./util/socket.io')(http);
 
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 // Custom error handler
 app.use(function(err, req, res, next) {
@@ -37,8 +39,6 @@ app.use(function(err, req, res, next) {
 	}
 });
 
-
-
 //
 // Routes
 //
@@ -47,13 +47,13 @@ var basePath = path.join(__dirname, '/routes/');
 fs.readdirSync(basePath).forEach(function(filename) {
 	var basePathService = '/' + filename.replace(/\.js$/, '');
 	var serviceDefinition = basePath + filename;
-	app.use(basePathService, require(serviceDefinition));
+	app.use(basePathService, require(serviceDefinition)(io));
 });
 
 var ip = config.server.ip;
 var port = config.server.port;
 
-app.listen(port, ip, function() {
+http.listen(port, ip, function() {
 	debug('Application listening on http://' + ip + ':' + port);
 });
 

3267433eaeccd46efbbc387e4579c1ee026c7257
IGZJavierPerez <surtich@gmail.com>
2015-02-09 23:23:12 +0100

    Added GoCD config

---
+++ server/config/gocd.js
@@ -0,0 +1,18 @@
+module.exports = {
+	server: {
+		ip: process.env.IP || '0.0.0.0',
+		port: process.env.PORT || 8000
+	},
+	db: {
+		conn: 'mongodb://' + (process.env.db || 'mongodb') + ':27017/socialscoreboard'
+	},
+	ids: {
+		google: {
+			client_id:'380454543382-fqmk59spk85djt38boca89rub00nl44t.apps.googleusercontent.com',
+			client_secret: 'VThoqqMepxiVtwwk54--B-M5',
+			callback_url: 'http://localhost:8000/auth/oauth2callback',
+			scopes: ['https://www.googleapis.com/auth/userinfo.profile']
+		}
+	},
+	jwtSecret: 'very insecure secret'
+};
